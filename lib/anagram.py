6715890594f9facc27baef7b92b91d7d77981856
chrisngili <ngilichristopher@gmail.com>
2025-05-12 16:17:13 +0300

    tests passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, wordlist):
+        self.wordlist = wordlist
+
+    def match(self, words):
+        anagrams = []
+        for word in words:    
+            if  sorted(word) == sorted(self.wordlist) and word != self.wordlist:
+                anagrams.append(word)
+        return anagrams
+
+
+print(Anagram("enlist").match(["listen", "poison", "hello"]))      
